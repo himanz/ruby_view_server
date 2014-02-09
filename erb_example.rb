9cbb52dbb4414e83e24e666c010e6cca5add484d
Jonathan Chang <jonoschang7@gmail.com>
2014-02-09 15:43:10 -0500

    Add 3 erb blocks to erb_example

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,21 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+word = 'hello'
+upword = word.upcase
+erb_string = "capitalized hello is: <%= upword %>"
+string_template = ERB.new(erb_string)
+puts string_template.result(binding)
+
+movies = {  "action" => "dark knight",
+	        "fantasy" => "lord of the rings",
+            "history" => "gladiator"
+}
+
+erb_hash = "my favorite fantasy movie is <%= movies['fantasy'] %>"
+hash_template = ERB.new(erb_hash)
+puts hash_template.result(binding)
+
+multiply_template = ERB.new "The value of x multiplied by 5 is: <%= x * 5 %>"
+puts multiply_template.result(binding)
